882fbd184db0da5315db2d990a2b79fba997393e
Sugarbell <ccruz4344@gmail.com>
2022-07-03 21:13:56 -0600

    ^q^

--- Sayuniq/plugins/send_video_stream_test.py
+++ Sayuniq/plugins/send_video_stream_test.py
@@ -13,12 +13,22 @@ class NamedBytesIO(BytesIO):
 @Client.on_message(filters.command(["up"]))
 async def __svst__(bot, update):
     print(update)
-    _orl = "https://cdn.donmai.us/original/39/27/__izayoi_sakuya_touhou__39279272c19a06b268fd40931ff29317.mp4"
+    _fmbd = "https://www.fembed.com/v/xgmqxh571yg2m03"
     async with aiohttp.ClientSession() as request:
-        async with request.get(_orl) as r:
+        async with request.get(_fmbd) as r1:
+            _host = r1.host
+        async with request.post(f"https://{_host}/api/source/" + _fmbd.split("/")[-1]) as r:
+            rspns_json = await r.json()
+            _orl = rspns_json["data"][-1]["file"]
+        async with request.get(_orl) as r2:
             await bot.send_video(
                 TESTS_CHANNEL,
-                NamedBytesIO(await r.content.read(), "file")
+                # NamedBytesIO(await r.content.read(), "file")
+                BytesIO(await r2.content.read()),
+                file_name="@Japanemision"
             )
+
+    # _orl = "https://cdn.donmai.us/original/39/27/__izayoi_sakuya_touhou__39279272c19a06b268fd40931ff29317.mp4"
+    # async with aiohttp.ClientSession() as request:
 
 
