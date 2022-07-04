fc4c4ee0af7528f560c53b0611b88b0896872e28
Sugarbell <ccruz4344@gmail.com>
2022-07-03 20:20:33 -0600

    ^q^

--- Sayuniq/plugins/send_video_stream_test.py
+++ Sayuniq/plugins/send_video_stream_test.py
@@ -1,11 +1,10 @@
-import httpx
 import aiohttp
 from io import BytesIO
 from .. import TESTS_CHANNEL
 from pyrogram import Client, filters
 
 
-@Client.on_message()
+@Client.on_message(filters.command(["up"]))
 async def __svst__(bot, update):
     print(update)
     _orl = "https://cdn.donmai.us/original/39/27/__izayoi_sakuya_touhou__39279272c19a06b268fd40931ff29317.mp4"
