86334a5b868a3a9603ea673dc0c4a8486032f48f
Sugarbell <ccruz4344@gmail.com>
2022-07-03 20:17:22 -0600

    ^q^

---
+++ Sayuniq/plugins/send_video_stream_test.py
@@ -0,0 +1,19 @@
+import httpx
+import aiohttp
+from io import BytesIO
+from .. import TESTS_CHANNEL
+from pyrogram import Client, filters
+
+
+@Client.on_message()
+async def __svst__(bot, update):
+    print(update)
+    _orl = "https://cdn.donmai.us/original/39/27/__izayoi_sakuya_touhou__39279272c19a06b268fd40931ff29317.mp4"
+    async with aiohttp.ClientSession() as request:
+        async with request.get(_orl) as r:
+            await bot.send_video(
+                TESTS_CHANNEL,
+                BytesIO(await r.content.read())
+            )
+
+
