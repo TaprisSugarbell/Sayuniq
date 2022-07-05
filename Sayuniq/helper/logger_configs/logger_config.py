6d3adf9926f2e72e5d165bc4840d216916fe371d
Sugarbell <ccruz4344@gmail.com>
2022-07-05 16:57:42 -0600

    ^q^

---
+++ Sayuniq/helper/logger_configs/logger_config.py
@@ -0,0 +1,62 @@
+from Sayuniq.__vars__ import BOT_NAME, LOGGING_LEVEL
+
+__dr, __file = "./logs/", f"{BOT_NAME}.log"
+log_file = __dr + __file
+
+_dbt = "-----------------------------------------------------------" \
+       "------------------------------------------------------------"
+
+LOGGER_CONFIG = {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "formatters": {
+            "default": {
+                "format": "[%(levelname)s || %(asctime)s] %(name)s: %(message)s"
+            },
+            BOT_NAME: {
+                "format": f"{_dbt}\n[%(levelname)s || %(hhr)s] REASON = \"%(message)s\"\n"
+            }
+        },
+        "handlers": {
+            "default": {
+                "level": "INFO",
+                "formatter": "default",
+                "class": "logging.StreamHandler",
+                # "stream": "ext://sys.stdout"
+            },
+            "file": {
+                  "class": "logging.handlers.RotatingFileHandler",
+                  "filename": log_file,
+                  "maxBytes": 3145728,
+                  "backupCount": 1,
+                  "formatter": "default"
+                }
+        },
+        "loggers": {
+            "": {
+                "handlers": [
+                    "default", "file"
+                ],
+                "level": "INFO",
+                "propagate": False
+            },
+            BOT_NAME: {
+                "handlers": [
+                    "default", "file"
+                ],
+                "level": LOGGING_LEVEL,
+                "propagate": False
+            },
+            # "__main__": {
+            #     "handlers": [
+            #         "default"
+            #     ],
+            #     "level": "DEBUG",
+            #     "propagate": False
+            # }
+        }
+    }
+
+
+
+
--- Sayuniq/plugins/send_video_stream_test.py
+++ Sayuniq/plugins/send_video_stream_test.py
@@ -1,9 +1,14 @@
-import cv2
+import time
+import numpy
+import random
+import ffmpeg
 import aiohttp
+from PIL import Image
 from io import BytesIO
+from .. import sayulog
 from .. import TESTS_CHANNEL
 from pyrogram import Client, filters
-from moviepy.video.io.VideoFileClip import VideoFileClip
+from ..__vars__ import human_hour_readable
 
 
 class NamedBytesIO(BytesIO):
@@ -15,6 +20,7 @@ class NamedBytesIO(BytesIO):
 @Client.on_message(filters.command(["up"]))
 async def __svst__(bot, update):
     print(update)
+    sayulog.warning("NO SE SI FUNCIONARA!!!", extra={"hhr": human_hour_readable()})
     _fmbd = "https://www.fembed.com/v/xgmqxh571yg2m03"
     async with aiohttp.ClientSession() as request:
         async with request.get(_fmbd) as r1:
@@ -26,15 +32,38 @@ async def __svst__(bot, update):
             print(r2.headers)
             print(r2.content_length)
             print(r2.request_info)
-            data = cv2.VideoCapture(_orl)
-            frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
-            fps = int(data.get(cv2.CAP_PROP_FPS))
-            seconds = int(frames / fps)
-            print(seconds)
-            clip = VideoFileClip(_orl)
+            _start = time.time()
+            _data = ffmpeg.probe(_orl)
+            _data_of_video = [stream for stream in _data["streams"] if stream["codec_type"] == "video"][0]
+            print(_data)
+            print(_data_of_video)
+            # DATA
+            width = _data_of_video["width"]
+            height = _data_of_video["height"]
+            duration = _data_of_video["duration"]
+            # total_frames = int(_data_of_video['nb_frames'])
+            print("Waiting... {}".format(time.time() - _start))
+            _ss = int(float(duration) / random.randint(15, 30))
+            frame, err = (
+                ffmpeg.input(_orl, ss=_ss)
+                .output('pipe:', vframes=1, format='image2', vcodec='png')    # vcodec=mjpeg || png
+                .run(quiet=True)
+                # .run(capture_stdout=True)
+            )
+            # nimage = Image.open(BytesIO(frame))
+            # nimage.save("screenshot-0.png")
+            # print(f"Waiting-2... {time.time() - _start} :3")
+
+            # data = cv2.VideoCapture(_orl)
+            # frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
+            # fps = int(data.get(cv2.CAP_PROP_FPS))
+            # seconds = int(frames / fps)
+            # print(seconds)
+
+            # clip = VideoFileClip(_orl)
             # Extraer información del video
-            width, height = clip.size
-            duration = int(clip.duration)
+            # width, height = clip.size
+            # duration = int(clip.duration)
             print(width, height, duration)
             await bot.send_video(
                 TESTS_CHANNEL,
@@ -43,7 +72,8 @@ async def __svst__(bot, update):
                 file_name="@Japanemision",
                 duration=duration,
                 width=width,
-                height=height
+                height=height,
+                thumb=BytesIO(frame)
             )
 
     # _orl = "https://cdn.donmai.us/original/39/27/__izayoi_sakuya_touhou__39279272c19a06b268fd40931ff29317.mp4"
